062a1c4e22d6647a479d2da87db057760221d0b1
Jmuinde <jmuindedt@gmail.com>
2025-05-29 21:00:18 +0300

    updated the dag file

--- dags/weather_etl_dag.py
+++ dags/weather_etl_dag.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 """
 AirflowDag module
+Makes use of the taskAPI to facilate data transfer 
+between tasks
 """
 import logging
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+from airflow.models import Variable
 from datetime import datetime, timedelta
 from ingestion.fetch_weather_data import fetch_weather
 from processing.transform_weather_data import   transform_weather_data
@@ -16,62 +19,46 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
+# Get Cities
+@task
+def get_city_list():
+     default_cities =["Nairobi","Machakos","Mombasa","London","Kisumu"]
+     cities_str = Variable.get("weather_cities", default_var=",".join(default_cities))
+     cities = [city.strip() for city in cities_str.split(",") if city.strip()]
+     logging.info(f"[Config] Cities to fecth: {cities}")
+     return cities
 # Task 1 - Extract data 
-
-def extract_data(**context):
-    cities = ["Nairobi", "Machakos", "Kisumu"]
+@task
+def extract_data(cities):
     all_raw_data = []
     for city in cities:
         raw_data = fetch_weather(city)
-        logging.info(f"Fetched raw data: {raw_data}")
+        logging.info(f"[Extract] fetched raw data for {city}: {raw_data}")
         all_raw_data.append(raw_data)
+    return all_raw_data
 
-    # Push data to XCom
-    context['ti'].xcom_push(key='raw_weather_data', value=all_raw_data)
-
-# Task 2: Transform data 
-def transform_data(**context):
-        
-        raw_data = context['ti'].xcom_pull(task_ids='extract_task', key='raw_weather_data')
-        clean_data = transform_weather_data(raw_data)
-        logging.info(f"Transformed data: {clean_data}")
-
-        # Push cleaned data to XCom
-        context['ti'].xcom_push(key='clean_weather_data', value=clean_data)
-    
+# Task 2: Transform data
+@task
+def transform_data(raw_data):
+    clean_data = transform_weather_data(raw_data)
+    logging.info(f"[Transform] Cleaned weather data: {clean_data}")
+    return clean_data
 
 # Task 3: Load data 
-
-def load_data(**context):
-     clean_data = context['ti'].xcom_pull(task_ids='transform_task', key='clean_weather_data')
+@task
+def load_data(clean_data):
      load_weather_data(clean_data)
-     logging.info("loaded data to Postgresql")
+     logging.info("[Load] Weather data loaded to Postgresql db")
 
 # Define the DAG and TASKS
 with DAG (
     dag_id='weather_etl_pipeline',
     default_args=default_args,
-    start_date=datetime(2025,5,23),
+    start_date=datetime(2025,5,29),
     schedule='*/5 * * * *', # Every 5 minutes
     catchup=False
 ) as dag:
-
-    task_1 = PythonOperator(
-        task_id='extract_task',
-        python_callable=extract_data,
-        provide_context=True
-    )
-
-    task_2 = PythonOperator(
-         task_id='transform_task',
-         python_callable=transform_data,
-         provide_context=True
-    )
-
-    task_3 = PythonOperator(
-         task_id='load_task',
-         python_callable=load_data,
-         provide_context=True
-    )
-
-    task_1 >> task_2 >> task_3
+    cities = get_city_list()
+    raw = extract_data(cities)
+    clean = transform_data(raw)
+    load_data(clean)
---
+++ dags/weather_etl_dag_backup.py
@@ -0,0 +1,83 @@
+"""
+#!/usr/bin/python
+
+AirflowDag module
+This Module uses the custom xcom appraoch
+to transfer data between tasks.
+The main module weather_etl_dag makes use of the taskAPI
+which is more convinient. 
+
+import logging
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+from ingestion.fetch_weather_data import fetch_weather
+from processing.transform_weather_data import   transform_weather_data
+from storage.load_to_postgres import load_weather_data
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+# Task 1 - Extract data 
+
+def extract_data(**context):
+    cities = ["Nairobi", "Machakos", "Kisumu"]
+    all_raw_data = []
+    for city in cities:
+        raw_data = fetch_weather(city)
+        logging.info(f"Fetched raw data: {raw_data}")
+        all_raw_data.append(raw_data)
+
+    # Push data to XCom
+    context['ti'].xcom_push(key='raw_weather_data', value=all_raw_data)
+
+# Task 2: Transform data 
+def transform_data(**context):
+        
+        raw_data = context['ti'].xcom_pull(task_ids='extract_task', key='raw_weather_data')
+        clean_data = transform_weather_data(raw_data)
+        logging.info(f"Transformed data: {clean_data}")
+
+        # Push cleaned data to XCom
+        context['ti'].xcom_push(key='clean_weather_data', value=clean_data)
+    
+
+# Task 3: Load data 
+
+def load_data(**context):
+     clean_data = context['ti'].xcom_pull(task_ids='transform_task', key='clean_weather_data')
+     load_weather_data(clean_data)
+     logging.info("loaded data to Postgresql")
+
+# Define the DAG and TASKS
+with DAG (
+    dag_id='weather_etl_pipeline',
+    default_args=default_args,
+    start_date=datetime(2025,5,23),
+    schedule='*/5 * * * *', # Every 5 minutes
+    catchup=False
+) as dag:
+
+    task_1 = PythonOperator(
+        task_id='extract_task',
+        python_callable=extract_data,
+        provide_context=True
+    )
+
+    task_2 = PythonOperator(
+         task_id='transform_task',
+         python_callable=transform_data,
+         provide_context=True
+    )
+
+    task_3 = PythonOperator(
+         task_id='load_task',
+         python_callable=load_data,
+         provide_context=True
+    )
+
+    task_1 >> task_2 >> task_3
+"""
